717ee9186b76ff8c0462da98fd73aec217d9946b
chef-vulcan <lilcrystalhimself@gmail.com>
2025-06-14 17:20:14 +0100

    modified the index.html, style.css, and index.js file

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 // The JavaScript calculator code
 
+
+//This was used to get the id="dispay" on the html file using the DOM. 
 const display = document.getElementById("display")
 
+
+//This function appends the input that was clicked on the button to the display
 function appendToDisplay(input){
-
+    display.value += input;
 }
 
-function clearDisply(){
-
+//This function is used to delete one input or value/the last value on the display whenever the button is being clicked
+function deleteOneOnDisplay(){
+    display.value = display.value.slice(0, -1);
 }
 
+//This function clears everything on the display
+function wipeDisplay(){
+    display.value = ""
+}
+
+
+//This function evaluates the result of the input passed and displays the result
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+
+    catch(error){
+        display.value = "Error"
+    }
+
+}
